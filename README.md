e150b61eb96da45b70b86ae01db7aab0e4d68793
Kyle Flavin <Kyle.Flavin@citrix.com>
2016-12-27 16:23:54 -0800

    Update README with instructions on usage, add boto (possibly not necessary, but seem to need boto not boto3 to delete instance profiles), and Cloudformation JSON template for testing.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # cfn-python-lambda
+
+An example of an AWS Cloudformation, lambda backed custom resource using Python. 
+
+```bash
+git clone git@github.com:kflavin/cfn-python-lambda.git
+cd cfn-python-lambda
+zip -r ../cfn-python-lambda.zip .
+aws lambda update-function-code --function-name CreateIAMResources --zip-file fileb://../pylambda.zip
+aws lambda create-function --function-name cfn-python-lambda --runtime python2.7 --role <role ARN> --handler lambda_function.lambdahandler --zip-file fileb://../cfn-python-lambda.zip
+```
+
+The role should have necessary IAM permissions to create an instance profile.
--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,41 @@
 import httplib
 import urlparse
 import json
-import boto3
+import boto
+from boto.exception import BotoServerError
 
-from cfnresponse import send, SUCCESS
+from cfnresponse import send, SUCCESS, FAILED
 
 def lambda_handler(event, context):
+    ## allow CF template to be deleted easily if we make a mistake
+    #send(event, context, SUCCESS)
+    #return
+
+    print("My event is " + str(event))
+    print("My event type is " + str(type(event)))
+
+    instance_profile_name = event['ResourceProperties']['InstanceProfileName']
+
+    iam = boto.connect_iam()
+    status = SUCCESS
+
     if event['RequestType'] == 'Delete':
-        send(event, context, SUCCESS)
+        try:
+            iam.delete_instance_profile(instance_profile_name)
+        except BotoServerError as e:
+            print("Could not find instance profile, assuming it has been removed "+ str(e))
+            status=SUCCESS
+
+        send(event, context, status)
         return
 
-    data = {'key1': 'value1'}
+    # Example of returning data
+    data = {'InstanceProfileName': instance_profile_name}
 
-    send(event, context, SUCCESS, response_data=data)
+    try:
+        iam.create_instance_profile(instance_profile_name)
+    except BotoServerError as e:
+        print("Failed to create "+ str(e))
+        status = FAILED
+
+    send(event, context, status, response_data=data)
